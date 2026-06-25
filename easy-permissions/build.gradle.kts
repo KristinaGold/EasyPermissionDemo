80af7a0c0bf0bbe5da50c9353d780b29ae6ac179
KristinaGold <kristinagoldin@gmail.com>
2026-06-25 17:42:10 +0300

    /

--- easy-permissions/build.gradle.kts
+++ easy-permissions/build.gradle.kts
@@ -55,7 +55,7 @@ afterEvaluate {
             register<MavenPublication>("release") {
                 groupId = "com.github.KristinaGold"
                 artifactId = "easy-permissions"
-                version = "1.0.6"
+                version = "1.0.7"
 
                 from(components["release"])
             }
